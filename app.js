f44d279248c450716f4b7745670d99b793ddff39
iAmmar7 <maansaridot@gmail.com>
2019-05-28 15:05:42 +0500

    server is running

--- app.js
+++ app.js
@@ -13,4 +13,28 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO"
+let interval;
+
+io.on("connection", socket => {
+  console.log("New client connected");
+  if(interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  })
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get (
+      "https://api.darksky.net/forecast/f99e836076c70a46d407fa40c22a8f62/37.8267,-122.4233"
+    );
+    socket.emit("FromAPI", res.data.currently.temperature);
+  } catch(error) {
+    console.log(`Error: ${error.code}`);
+  }
+};
